c91ee2c8ef42c52c6a8c6a472cdbccbabec54af2
Shubh <shubhjhawar78@gmail.com>
2023-11-26 21:01:51 -0500

    img

---
+++ form/src/assets/index.js
@@ -0,0 +1,8 @@
+import leftBackground from "./bg-sidebar-desktop.svg";
+import arcade from "./icon-arcade.svg";
+import advanced from "./icon-advanced.svg";
+import checkmark from "./icon-checkmark.svg";
+import pro from "./icon-pro.svg";
+import thanks from "./icon-thank-you.svg";
+
+export { leftBackground, arcade, advanced, checkmark, pro, thanks};
